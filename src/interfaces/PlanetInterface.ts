914d30673802dfe2eafa125fcfd171f8fe984a98
andrefelix <afelixfreitas@gmail.com>
2020-01-21 14:29:08 -0300

    :ok_hand: Interface para Planet, desenhado para facilitar acesso aos dados

--- src/interfaces/PlanetInterface.ts
+++ src/interfaces/PlanetInterface.ts
@@ -1,7 +1,12 @@
+/**
+ * - Interface que descreve as propriedades de um Planet.
+ *
+ * @interface
+ */
 export default interface PlanetInterface {
   name: string
   population: number | undefined
-  climate: string
-  terrain: string
+  climate: string[]
+  terrain: string[]
   featuredInFilms: string[]
 }
