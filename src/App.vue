2bf3d6d3a77d76d9a76edd45c0eb67e4cde126d8
andrefelix <afelixfreitas@gmail.com>
2020-01-21 14:32:22 -0300

    :ok_hand: Estilizado e componentizado o componente root (App.vue)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Planet />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Planet from './components/Planet.vue'
 
 @Component({
   components: {
-    HelloWorld
+    Planet
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+@import url(https://fonts.googleapis.com/css?family=Roboto&display=swap);
+
+:root {
+  --main-bg-color: #fff;
+  --primary-color: #808080;
+  --secondary-color: #a0a0a0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #fff;
+  color: #808080;
+  font-size: 1.2rem;
+  font-weight: 700;
   margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 </style>
